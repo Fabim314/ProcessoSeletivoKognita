517ff2e117db2ec3a83eb79541047f09a91cbcec
Fábio Alan Martins Pinto <fabio.alan@neofield.com.br>
2022-01-31 20:01:28 -0300

    Fix correct output format, add comments and deal with the case of invalid input format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from "react";
-import logo from  "./images/kognita_logo.png" 
+import logo from  "./images/kognita_logo.png";
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState("")
-  const [result, setResult] = useState([0, 0])
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState([-1, 1]);
   const [showResult, setShowResult] = useState(false);
 
+  // Caso algo seja escrito na caixa de texto, esconder o resultado, pois ele não seria mais verdadeiro
   useEffect(() => {
-    setShowResult(false)
+    setShowResult(false);
   }, [input]);
 
+  // A função em si a ser implementada
   const compareValues = () => {
-    const arr = input.split('\n')
+    // Separa o input em dois, uma vez que os escores são separados por um 'enter'
+    const arr = input.split('\n');
     const a = arr[0].split(' ').map((score) => {return parseInt(score)});
     const b = arr[1].split(' ').map((score) => {return parseInt(score)});
     let result = [0, 0];
+    if(a.length !== 3 || b.length !== 3){
+      alert("Input em um formato inválido!");
+      return;
+    }
     a.forEach((_, index) => a[index] > b[index] ? result[0]++ : (b[index] > a[index] ? result[1]++ : null));
-    setResult(result)
-    setShowResult(true)
+    setResult(result);
+    setShowResult(true);
   }
 
   return (
@@ -29,13 +36,13 @@ function App() {
       </header>
       <div className="body">
         <span>Insira as notas da Rafaela e do Mateus, respectivamente.</span>
-        <span> Separe as pontuações de cada um com enter:</span>
+        <span>Separe a pontuação da Rafaela da do Mateus um com enter:</span>
           <textarea className="text-input" value={input} onChange={(e) => setInput(e.target.value)} rows="2">
           </textarea>
           <button className="submit-btn" onClick={() => compareValues()}>Enviar resultados</button>
         {showResult ? <div>
           <p>Pontuação final:</p>
-          <span>{"[" + result[0] + ", " + result[1] + "]"}</span>
+          <span>{result[0] + " " + result[1]}</span>
         </div> : null}
       </div>
     </div>
